7570853529ead61e44ec77fe0647791f1213570b
RobinVanRoy <vanroyrobin@gmail.com>
2017-09-07 16:00:00 +0200

    functionaliteit add recept

--- Examenvraag2/recepten/urls.py
+++ Examenvraag2/recepten/urls.py
@@ -4,16 +4,6 @@ from recepten import views
 app_name = 'recepten'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    # ex: everything/car/1  --> detailview
-    url(r'^car/(?P<pk>[0-9]+)/$', views.CarDetailView.as_view(), name='cardetail'),
-    # ex: everything/carbrand/1  --> detailview
-    url(r'^carbrand/(?P<pk>[0-9]+)/$', views.CarbrandDetailView.as_view(), name='carbranddetail'),
-    # ex: everything/car/create  --> createview
-    url(r'^car/create/$', views.CarCreateView.as_view(), name='carcreate'),
-    # ex: everything/carbrand/create  --> createview
-    url(r'^carbrand/create/$', views.CarbrandCreateView.as_view(), name='carbrandcreate'),
-    # ex: everything/car/update  --> updateview
-    url(r'^car/update/(?P<pk>[0-9]+)/$', views.CarUpdateView.as_view(), name='carupdate'),
-    # ex: everything/car/delete/1  --> deleteview
-    url(r'^car/delete/(?P<pk>[0-9]+)/$', views.CarDeleteView.as_view(), name='cardelete')
+    url(r'^create/$', views.create, name='create'),
+    url(r'^recept/create/$', views.createrecept, name='createrecept'),
 ]
--- Examenvraag2/recepten/views.py
+++ Examenvraag2/recepten/views.py
@@ -3,10 +3,14 @@ from pymongo import MongoClient
 
 
 def connection():
+    # mongo1 = pymongo.Connection('127.0.0.1')
+
+
     # Create a new connection to a single MongoDB instance at host:port.
     connection = MongoClient('localhost', 27017)
+    # print connection.database_names()
     # Create and get a data base (db) from this connection
-    db = connection.test
+    db = connection.receptenDB
     # Create and get a collection named "recepten" from the data base named "db" from the connection named "connection"
     collection = db.recepten
     connection.close()
@@ -14,10 +18,38 @@ def connection():
 
 
 def index(request):
-    recepten = connection()
-    recepten = recepten.sort('$name', -1).sort('$calorien', -1)
+    collection = connection()
+    recepten = collection.find().sort('$name', -1).sort('$calorien', -1)
     context = {'recepten': recepten}
-    print(context)
+    # print(context)
     return render(request, 'recepten/index.html', context)
+
+
+def create(request):
+    return render(request, 'recepten/create.html')
+
+
+def createrecept(request):
+    name = request.POST.get('input_naam')
+    time = request.POST.get('input_tijd')
+    calorien = request.POST.get('input_calorien')
+    ingredienten = request.POST.get('input_ingredienten')
+
+    connection = MongoClient('localhost', 27017)
+    db = connection.receptenDB
+
+    db.recepten.insert_one({
+        "name": name,
+        "calorien": calorien,
+        "time": time,
+        "ingredienten": ingredienten
+    })
+
+    collection = db.recepten
+    
+    recepten = collection.find().sort('$name', -1).sort('$calorien', -1)
+    context = {'recepten': recepten}
+    return render(request, 'recepten/index.html', context)
+
 
 
